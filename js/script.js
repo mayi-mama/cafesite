c7b0c2af4c23957ff7b9c12bcb1adac70d15ce59
mayi-mama <mayimuro8845@gmail.com>
2023-04-23 00:10:47 +0900

    cssとjs追記

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 //ハンバーガーメニュー
 const hamburger = document.querySelector('#check');
-const gnav =document.querySelector('.gnav');
+const gnav = document.querySelector('.gnav');
 
 hamburger.addEventListener('click',function(){
-    hamburger.classList.toggle('active');
+   // hambuger.classList.toggle('active');
     gnav.classList.toggle('active');
 
+});
+
+
+//swiper
+const swiper = new Swiper(".swiper",{
+    loop:true,
+    autoplay:true,
+    pagination: {
+        el: ".swiper-pagination", 
+        type:'bullets'
+    },
 });
